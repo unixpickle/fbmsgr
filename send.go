793432522ecb7de88350e00ab81296e09d1f778f
Alex Nichol <unixpickle@gmail.com>
2016-10-26 09:58:18 -0400

    simplify SendText post fields

--- send.go
+++ send.go
@@ -2,39 +2,30 @@ package fbmsgr
 
 import (
 	"math/rand"
-	"net/url"
 	"strconv"
+	"time"
 )
 
 // SendText attempts to send a textual message to the user
 // with the given fbid.
 func (s *Session) SendText(fbid, message string) error {
-	dtsg, err := s.fetchDTSG()
+	reqParams, err := s.commonParams()
 	if err != nil {
 		return err
 	}
-
-	reqParams := url.Values{}
-	reqParams.Add("__a", "1")
-	reqParams.Add("__af", "o")
-	reqParams.Add("__be", "-1")
-	reqParams.Add("__pc", "EXP1:messengerdotcom_pkg")
-	reqParams.Add("__req", "14")
-	reqParams.Add("__rev", "2643465")
-	reqParams.Add("__srp_t", "1477432416")
-	reqParams.Add("__user", s.userID)
 	reqParams.Add("action_type", "ma-type:user-generated-message")
 	reqParams.Add("body", message)
-	reqParams.Add("client", "mercury")
 	reqParams.Add("ephemeral_ttl_mode", "0")
-	reqParams.Add("fb_dtsg", dtsg)
 	reqParams.Add("has_attachment", "false")
 	msgID := randomMessageID()
 	reqParams.Add("message_id", msgID)
 	reqParams.Add("offline_threading_id", msgID)
 	reqParams.Add("other_user_fbid", fbid)
 	reqParams.Add("source", "source:messenger:web")
-	reqParams.Add("timestamp", "1477433223060")
+
+	timestamp := time.Now().UnixNano() / 1000000
+	ts := strconv.FormatInt(timestamp, 10)
+	reqParams.Add("timestamp", ts)
 
 	res, err := s.client.PostForm(BaseURL+"/messaging/send/?dpr=1", reqParams)
 	if res != nil {
