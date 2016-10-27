7240b48c4d79a0ee661199f0e8c44ca393830d2d
Alex Nichol <unixpickle@gmail.com>
2016-10-27 16:57:58 -0400

    send messages to group chats

--- send.go
+++ send.go
@@ -1,37 +1,77 @@
 package fbmsgr
 
 import (
+	"encoding/json"
+	"errors"
 	"math/rand"
+	"net/url"
 	"strconv"
 	"time"
 )
 
 // SendText attempts to send a textual message to the user
 // with the given fbid.
-func (s *Session) SendText(fbid, message string) error {
+func (s *Session) SendText(fbid, message string) (msgID string, err error) {
+	reqParams, err := s.textMessageParams(message)
+	if err != nil {
+		return "", err
+	}
+	reqParams.Add("other_user_fbid", fbid)
+	return s.sendMessage(reqParams)
+}
+
+// SendGroupText is like SendText, but the message is sent
+// to a group chat rather than to an individual.
+func (s *Session) SendGroupText(groupFBID, message string) (msgID string, err error) {
+	reqParams, err := s.textMessageParams(message)
+	if err != nil {
+		return "", err
+	}
+	reqParams.Add("thread_fbid", groupFBID)
+	return s.sendMessage(reqParams)
+}
+
+func (s *Session) textMessageParams(body string) (url.Values, error) {
 	reqParams, err := s.commonParams()
 	if err != nil {
-		return err
+		return nil, err
 	}
+
 	reqParams.Add("action_type", "ma-type:user-generated-message")
-	reqParams.Add("body", message)
+	reqParams.Add("body", body)
 	reqParams.Add("ephemeral_ttl_mode", "0")
 	reqParams.Add("has_attachment", "false")
 	msgID := randomMessageID()
 	reqParams.Add("message_id", msgID)
 	reqParams.Add("offline_threading_id", msgID)
-	reqParams.Add("other_user_fbid", fbid)
 	reqParams.Add("source", "source:messenger:web")
 
 	timestamp := time.Now().UnixNano() / 1000000
 	ts := strconv.FormatInt(timestamp, 10)
 	reqParams.Add("timestamp", ts)
 
-	res, err := s.client.PostForm(BaseURL+"/messaging/send/?dpr=1", reqParams)
-	if res != nil {
-		res.Body.Close()
+	return reqParams, nil
+}
+
+func (s *Session) sendMessage(values url.Values) (mid string, err error) {
+	response, err := s.jsonForPost(BaseURL+"/messaging/send/?dpr=1", values)
+	if err != nil {
+		return "", err
 	}
-	return err
+	var obj struct {
+		Payload struct {
+			Actions []struct {
+				MessageID string `json:"message_id"`
+			} `json:"actions"`
+		} `json:"payload"`
+	}
+	json.Unmarshal(response, &obj)
+	for _, x := range obj.Payload.Actions {
+		if x.MessageID != "" {
+			return x.MessageID, nil
+		}
+	}
+	return "", errors.New("no message ID in response")
 }
 
 func randomMessageID() string {
